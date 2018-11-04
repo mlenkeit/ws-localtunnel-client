b8cbae073b2f8523ce42bf0dad69b7fa040e4884
Maximilian Lenkeit <mlenkeit@users.noreply.github.com>
2018-11-04 20:35:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ws-localtunnel-client
+
+[Localtunnel](https://github.com/localtunnel/localtunnel) inspired client based on web sockets
+
+See [ws-localtunnel-server](https://github.com/mlenkeit/ws-localtunnel-server) for the corresponding server.
---
+++ client.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const assert = require('assert')
+const request = require('request')
+
+const WebSocketClient = require('websocket').client
+
+module.exports = function (opts) {
+  assert(opts.api_token, 'API token option is required')
+  assert(opts.host, 'host option is required')
+  assert(opts.port, 'port option is required')
+  assert(opts.realm, 'realm option is required')
+
+  const client = new WebSocketClient()
+
+  client.on('connectFailed', function (error) {
+    console.error('Failed to connect:', error.toString())
+  })
+
+  client.on('connect', function (connection) {
+    console.log(`Connected to ${opts.host}, listening to realm ${opts.realm}`)
+
+    connection.on('error', function (error) {
+      console.error('Connection error:', error.toString())
+    })
+
+    connection.on('close', function () {
+      console.log('Connection was closed')
+    })
+
+    connection.on('message', function (message) {
+      if (message.type !== 'utf8') {
+        console.error(`Ignoring message of type ${message.type}`)
+      }
+
+      const reqMetadata = JSON.parse(message.utf8Data)
+      const uuid = reqMetadata.uuid
+      request({
+        method: reqMetadata.method,
+        uri: `http://localhost:${opts.port}${reqMetadata.url}`,
+        headers: reqMetadata.headers
+      }, function (err, res, body) {
+        if (err) {
+          return console.error(`Received error ${err.toString()}`)
+        }
+        connection.sendUTF(JSON.stringify({
+          uuid: uuid,
+          statusCode: res.statusCode,
+          headers: res.headers,
+          body: body
+        }))
+      })
+    })
+
+    const ping = function () {
+      if (connection.connected) {
+        connection.sendUTF('ping')
+        setTimeout(ping, 1000)
+      }
+    }
+    ping()
+  })
+
+  client.connect(`ws://${opts.host}/receive/${opts.realm}`, 'echo-protocol', null, {
+    'Authorization': `token ${opts.api_token}`
+  })
+
+  return client
+}
